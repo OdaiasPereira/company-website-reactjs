03d1d41a1c2ea9d89567d1a6fa59d804b735ac85
Odaias Pereira <odaiaspereira@usp.br>
2023-12-22 16:20:52 -0300

    uyuyi

--- src/components/founders.js
+++ src/components/founders.js
@@ -1,97 +1,65 @@
 //Team section
 function Founders() {
   return (
-    <section id="team" className="team my-5">
-      <div className="container">
-        <div className="section-top_text mb-4 text-center">
-          <p className="first">TIME</p>
-          <h2 className="main-title py-2">
+    <section id="team" class="team my-5">
+      <div class="container">
+        <div class="section-top_text mb-4 text-center">
+          <p class="first">TIME</p>
+          <h2 class="main-title py-2">
             Nosso Comprometido <span>Time</span>
           </h2>
-          <p className="text fw-bold w-50 mx-auto">
-            Um time qualificado proporcionando uma organização da sua saúde
-          </p>
         </div>
-        <div className="content">
-          <div className="row my-3">
-            <div className="col-md-3">
-              <div className="column_tm rounded-3 py-3 text-center">
-                <div className="design-img">
-                  <img
-                    className="rounded-top-3"
-                    src={process.env.PUBLIC_URL + "/img/juarez.png"}
-                    alt="Img Team member"
-                  />
-                  <div className="icons p-2">
-                    <a
-                      href="https://www.github.com/seu_usuario_github"
-                      target="_blank"
-                    >
-                      <i className="fab fa-github me-3"></i>
-                    </a>
-                    <a
-                      href="https://www.instagram.com/seu_usuario_instagram"
-                      target="_blank"
-                    >
-                      <i className="fa-brands fa-instagram me-3"></i>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/seu_usuario_linkedin"
-                      target="_blank"
-                    >
-                      <i className="fa-brands fa-linkedin me-3"></i>
-                    </a>
+        <div class="content">
+            <div class="row my-3">
+
+              <div class="col-md-3">
+
+              <div class="founder-fading-container">
+
+                <div class="column_tm rounded-3 py-3 text-center">
+                  <div class="rounded-container">
+                    <img class="rounded-top-founder" src={process.env.PUBLIC_URL + "/img/juarez.png"} alt="Img Team member" width="300" height="300" />
+                    <div class="icons p-2">
+                      <a href="https://www.github.com/seu_usuario_github" target="_blank"><i class="fab fa-github me-3"></i></a>
+                      <a href="https://www.linkedin.com/in/seu_usuario_linkedin" target="_blank"><i class="fa-brands fa-linkedin me-3"></i></a>
+                    </div>
+                  </div>
+                  <div class="text shadow py-4">
+                      <p class="curriculum">Esta pessoa possui um extenso histórico profissional na área de...</p>
+                      <h5 class="name fw-bold">Juarez Culau</h5>
+                      <p class="pro">CEO</p>
                   </div>
                 </div>
-                <div className="text shadow py-4">
-                  <h5 className="name fw-bold">Juarez Culau</h5>
-                  <p className="pro ceo">CEO e desenvolvedor</p>
+
                 </div>
+
               </div>
-            </div>
-            <div className="col-md-3">
-              <div className="column_tm rounded-3 py-3 text-center">
-                <div className="design-img">
-                  <img
-                    className="rounded-top-3"
-                    src="./assets/img/team/team-1.jpg"
-                    alt="Img Team member"
-                  />
-                  <div className="icons p-2">
-                    <a
-                      href="https://twitter.com/seu_usuario_twitter"
-                      target="_blank"
-                    >
-                      <i className="fa-brands fa-twitter me-3"></i>
-                    </a>
-                    <a
-                      href="https://www.facebook.com/seu_usuario_facebook"
-                      target="_blank"
-                    >
-                      <i className="fa-brands fa-facebook-f me-3"></i>
-                    </a>
-                    <a
-                      href="https://www.instagram.com/seu_usuario_instagram"
-                      target="_blank"
-                    >
-                      <i className="fa-brands fa-instagram me-3"></i>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/seu_usuario_linkedin"
-                      target="_blank"
-                    >
-                      <i className="fa-brands fa-linkedin me-3"></i>
-                    </a>
+
+              <div class="col-md-3">
+
+              <div class="founder-fading-container">
+
+                <div class="column_tm rounded-3 py-3 text-center">
+                  <div class="rounded-container">
+                    <img class="rounded-top-founder" src={process.env.PUBLIC_URL + "/img/odaias.png"} alt="Img Team member" width="300" height="300" />
+                    <div class="icons p-2">
+                      <a href="https://twitter.com/seu_usuario_twitter" target="_blank"><i class="fa-brands fa-twitter me-3"></i></a>
+                      <a href="https://www.linkedin.com/in/seu_usuario_linkedin" target="_blank"><i class="fa-brands fa-linkedin me-3"></i></a>
+                    </div>
+                  </div>
+                  <div class="text shadow py-4">
+                      <p class="curriculum">Esta pessoa possui um extenso histórico profissional na área de...</p>
+                      <h5 class="name fw-bold">Odaias Pereira</h5>
+                      <p class="pro">CTO</p>
+
                   </div>
                 </div>
-                <div className="text shadow py-4">
-                  <h5 className="name fw-bold">Odaias Pereira</h5>
-                  <p className="pro cto">CEO e consultor de assuntos biológicos</p>
+
                 </div>
+
               </div>
+              {/* Remova o bloco de código correspondente à terceira imagem */}
             </div>
-            {
-          </div>
         </div>
       </div>
     </section>
